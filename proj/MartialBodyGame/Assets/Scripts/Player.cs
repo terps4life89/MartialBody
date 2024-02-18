baaedef9f53f5b158961cda9fab185f959605e5e
Вікторія Тарасюк <tarasiukviktoiia03@gmail.com>
2024-02-18 21:20:11 +0200

    a

--- proj/MartialBodyGame/Assets/Scripts/Player.cs
+++ proj/MartialBodyGame/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
@@ -6,34 +7,72 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float nullSpeed = 0;
     public Animator animator;
-    // Update is called once per frame
-    
+    public int attackCounter = 0; 
+    private float lastAttackTime;
+    private float resetTime = 1f;
+    private bool isAttacking = false;
+
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        try
         {
-            transform.Translate(transform.right * speed * Time.deltaTime);
-            animator.SetFloat("Move", Mathf.Abs(speed));
+            if (Input.GetButtonDown("Fire1") && !isAttacking)
+            {
+                isAttacking = true;
+                attackCounter++;
+
+                if (attackCounter > 2)
+                {
+                    attackCounter = 0;
+                }
+
+                animator.SetInteger("AtackCount", attackCounter);
+                lastAttackTime = Time.time;
+
+                Invoke("ResetAttack", .5f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(transform.right * speed * Time.deltaTime);
+                animator.SetFloat("Move", Mathf.Abs(speed));
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(transform.up * speed * Time.deltaTime);
+                animator.SetFloat("Move", Mathf.Abs(speed));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-transform.right * speed * Time.deltaTime);
+                animator.SetFloat("Move", Mathf.Abs(speed));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-transform.up * speed * Time.deltaTime);
+                animator.SetFloat("Move", Mathf.Abs(speed));
+            }
+            if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S))
+            {
+                animator.SetFloat("Move", nullSpeed);
+            }
+
+            if (Time.time - lastAttackTime > resetTime)
+            {
+                attackCounter = 0;
+                animator.SetInteger("AtackCount", attackCounter);
+            }
         }
-        else if(Input.GetKey(KeyCode.W))
+        catch (Exception ex)
         {
-            transform.Translate(transform.up * speed * Time.deltaTime);
-            animator.SetFloat("Move", Mathf.Abs(speed));
+            //
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-transform.right * speed * Time.deltaTime);
-            animator.SetFloat("Move", Mathf.Abs(speed));
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-transform.up * speed * Time.deltaTime);
-            animator.SetFloat("Move", Mathf.Abs(speed));
-        }
-        else
-        {
-            animator.SetFloat("Move", 0);
-        }
+    }
+
+    void ResetAttack()
+    {
+        isAttacking = false;
     }
 }
